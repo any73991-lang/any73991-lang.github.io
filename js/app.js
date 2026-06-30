3a6cf5ffbeaabda1d14d8a1e6a6e61e10bb57ea8
any73991-lang <any73991@users.noreply.github.com>
2026-06-30 16:19:33 +0800

    Update to self-keep-alive backend (27f2a1d8...)

--- js/app.js
+++ js/app.js
@@ -475,7 +475,7 @@ const FEE = 0.001;
 
 // ========== API ==========
 const API_BASE = location.hostname === 'any73991-lang.github.io'
-  ? 'https://ffc9dd5601254102973e4685bc4b2457.codebuddy.cloudstudio.run'
+  ? 'https://27f2a1d8902e4cb181790cd4dc8b4858.codebuddy.cloudstudio.run'
   : '';
 var _demoRegData = null;
 
